5726fd355d729173121525cdaa04b74cf9d87252
Bal Cortex <balcortex@hotmail.com>
2021-01-21 17:48:20 -0600

    add save to replaybuffer

--- src/replay_buffer.py
+++ src/replay_buffer.py
@@ -1,7 +1,9 @@
 import collections
-import numpy as np
-from typing import Tuple, Deque, List
+import pickle
 from collections import namedtuple
+from typing import Deque, List, Tuple
+
+import numpy as np
 
 Experience = namedtuple("Experience", ["obs", "action", "reward", "done", "new_obs"])
 ExperienceBatch = namedtuple(
@@ -47,3 +49,56 @@ class ReplayBuffer:
             np.array(dones, dtype=np.uint8),
             np.array(next_states),
         )
+
+    def save(self, filename: str) -> None:
+        with open(filename, "wb") as f:
+            pickle.dump(self.buffer, f, pickle.HIGHEST_PROTOCOL)
+
+    @classmethod
+    def from_file(cls, filename: str) -> None:
+        buffer = cls(1)
+        with open(filename, "rb") as f:
+            deq = pickle.load(f)
+        buffer.buffer = deq
+
+        return buffer
+
+
+if __name__ == "__main__":
+    import gym
+    import os
+    from src.experience import ExperienceSourceDiscountedSteps
+    from src.policies import RandomPolicy
+
+    env = gym.make("Pendulum-v0")
+    rand_policy = RandomPolicy(env)
+    exp_source = ExperienceSourceDiscountedSteps(
+        policy=rand_policy,
+        gamma=0.99,
+        n_steps=1,
+        steps=4,
+    )
+    buffer = ReplayBuffer(1_000)
+
+    for disc_exp in next(exp_source):
+        exp = Experience(
+            obs=disc_exp.state,
+            action=disc_exp.action,
+            reward=disc_exp.discounted_reward,
+            done=True if disc_exp.last_state is None else False,
+            new_obs=disc_exp.last_state,
+        )
+        buffer.append(exp)
+
+    print(buffer.buffer)
+
+    buffer.save("buffer.pkl")
+
+    del buffer
+
+    buffer = ReplayBuffer.from_file("buffer.pkl")
+
+    print()
+    print(buffer.buffer)
+
+    os.remove("buffer.pkl")
