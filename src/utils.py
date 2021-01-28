e661d31d7860c86f9d86c732104357d849f16917
Bal Cortex <balcortex@hotmail.com>
2021-01-28 16:48:41 -0600

    add grid_generator (utils)

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
+import itertools
 import json
-from typing import Dict, Sequence
+from typing import Any, Dict, Generator, Optional, Sequence, Union
 
 import numpy as np
 import pandas as pd
@@ -7,21 +8,22 @@ import pandas as pd
 from src.logger import logger
 
 
-def read_weather_csv(path: str) -> pd.DataFrame:
-    "Read a csv file and returns a DataFrame object"
+def read_weather_csv(
+    path: str, format: Optional[str] = "%d/%m/%Y %H:%M"
+) -> pd.DataFrame:
+    "Read a csv weather file and returns a DataFrame object"
     logger.info(f"Reading {path} . . .")
     df = pd.read_csv(path)
-    df["Date"] = pd.to_datetime(df["Date"], format="%d/%m/%Y %H:%M")
+    if format:
+        df["Date"] = pd.to_datetime(df["Date"], format=format)
+    else:
+        df["Date"] = pd.to_datetime(df["Date"])
     df = df.set_index("Date")
     return df
 
 
-# def clip_num(value: float, minimum: float = -np.inf, maximum: float = np.inf) -> float:
-#     "Clip the value between minimum and maximum parameters"
-#     return min(max(value, minimum), maximum)
-
-
 def save_dict(dic: Dict, path: str, verbose: bool = True) -> None:
+    "Save a dictionary as a JSON file"
     with open(path, "w") as f:
         f.write(json.dumps(dic))
     if verbose:
@@ -29,21 +31,30 @@ def save_dict(dic: Dict, path: str, verbose: bool = True) -> None:
 
 
 def load_dict(path: str) -> Dict:
+    "Read a JSON file as a dictionary"
     with open(path) as f:
         dic = json.loads(f.read())
     logger.info(f"Dictionary readed from {path}")
     return dic
 
 
-def mse(a: Sequence, b: Sequence) -> float:
+def mse(a: Sequence[Union[int, float]], b: Sequence[Union[int, float]]) -> float:
     "Calculate the mean square error of `a` and `b`"
     a = np.array(a)
     b = np.array(b)
     return ((a - b) ** 2).mean()
 
 
-def efficiency(a: Sequence, b: Sequence) -> float:
+def efficiency(a: Sequence[Union[int, float]], b: Sequence[Union[int, float]]) -> float:
     "Calculate the efficiency of `b` with respect to `a`"
     a = np.array(a)
     b = np.array(b)
     return (b / a).mean() * 100
+
+
+def grid_generator(
+    dic: Dict[Any, Sequence[Any]]
+) -> Generator[Dict[Any, Any], None, None]:
+    "Perform permutation on the values (sequence) of a dictionary"
+    keys, values = zip(*dic.items())
+    return (dict(zip(keys, v)) for v in itertools.product(*values))
